2091f252198e83469fd4059eff0a97501d5913a3
Thepian <henrik@thepia.com>
2012-08-23 23:31:50 +0200

    test basic enhance/discard cycle

--- test/elements.js
+++ test/elements.js
@@ -31,7 +31,7 @@ function createDocument(html) {
 	// 	document.implementation.createDocumentType('html','',''));
 	var doc;
 	if (document.implementation && document.implementation.createHTMLDocument) {
-		doc = document.implementation.createHTMLDocument();
+		doc = document.implementation.createHTMLDocument("");
 	} else  if (window.ActiveXObject) {
 		doc = new ActiveXObject("htmlfile");
 	} else {
@@ -46,26 +46,90 @@ function createDocument(html) {
 	return doc;
 }
 
-test('Enhancing DocumentRoles',function(){
+test('Enhanced element config',function(){
+	ok(true,"TODO configure just using data-role");
+	ok(true,"TODO configure just using application/config");
+	ok(true,"TODO configure combining a base from application/config and a mixin using data-role");
+});
+
+test('Enhance element early or delayed',function() {
 	var DocumentRoles = Resolver("essential")("DocumentRoles");
 
 	var handlers = {
 		"enhance": {
-			"sinon": sinon.spy(),
+			"early": sinon.stub(),
+			"delayed": sinon.stub()
+		},
+		"layout": {
+			"early": sinon.stub(),
+			"delayed": sinon.stub()
+		},
+		"discard": {
+			"early": sinon.stub(),
+			"delayed": sinon.stub()
+		}
+	};
+
+	var doc = createDocument([
+		"<body>",
+		'<span role="delayed" id="a"></span>',
+		'<span role="early" id="b"></span>',
+		"</body>"
+		]);
+	handlers.enhance.delayed.returns(false);
+	var dr = DocumentRoles(handlers,doc);
+
+	var sinonConfig = {}; //TODO config for the sinon elem
+
+	equal(handlers.enhance.early.callCount,1);
+	equal(handlers.enhance.delayed.callCount,1);
+	equal(handlers.layout.early.callCount,0);
+	equal(handlers.discard.early.callCount,0);
+
+	ok(handlers.enhance.early.alwaysCalledWith(doc.getElementById("b"),"early"))
+	//TODO equal(handlers.enhance.early.args[0][2],sinonConfig);
+
+	ok(true,"TODO additional handlers, enhance call after initial round")
+
+	handlers.enhance.additional = sinon.stub();
+	handlers.enhance.delayed.returns({});
+	dr._enhance_descs();
+	equal(handlers.enhance.early.callCount,1,"enhance should be completed already");
+	equal(handlers.enhance.delayed.callCount,2);
+	equal(handlers.layout.early.callCount,0);
+	equal(handlers.discard.early.callCount,0);
+
+
+	//TODO all with roles are enhanced
+	//TODO discard called for all enhanced
+
+	DocumentRoles.info.constructors[-1].discarded(dr); // emulate singleton teardown
+	equal(handlers.enhance.early.callCount,1);
+	equal(handlers.enhance.delayed.callCount,2);
+	equal(handlers.layout.early.callCount,0);
+	equal(handlers.discard.early.callCount,1);
+	equal(handlers.discard.delayed.callCount,1);
+});
+
+//TODO layout and discard are optional handlers
+
+test('Enhancing DocumentRoles with builtin handlers',function(){
+	var DocumentRoles = Resolver("essential")("DocumentRoles");
+
+	var handlers = {
+		"enhance": {
 			"dialog": DocumentRoles.enhance_dialog,
 			"navigation": DocumentRoles.enhance_toolbar,
 			"spinner": DocumentRoles.enhance_spinner,
 			"application": DocumentRoles.enhance_application
 		},
 		"layout": {
-			"sinon": sinon.spy(),
 			"dialog": DocumentRoles.layout_dialog,
 			"navigation": DocumentRoles.layout_toolbar,
 			"spinner": DocumentRoles.layout_spinner,
 			"application": DocumentRoles.layout_application
 		},
 		"discard": {
-			"sinon": sinon.spy(),
 			"dialog": DocumentRoles.discard_dialog,
 			"navigation": DocumentRoles.discard_toolbar,
 			"spinner": DocumentRoles.discard_spinner,
@@ -80,22 +144,15 @@ test('Enhancing DocumentRoles',function(){
 		'<button name="a"></button>',
 		'</span>',
 
-		'<span role="sinon"></span>',
 		"</body>"
 		]);
 	var dr = DocumentRoles(handlers,doc);
 
-	equal(handlers.enhance.sinon.callCount,1);
-	equal(handlers.layout.sinon.callCount,0);
-	equal(handlers.discard.sinon.callCount,0);
 
-	//TODO test delayed enhance
-
-	// enhance should be completed
 	dr._enhance_descs();
-	equal(handlers.enhance.sinon.callCount,1);
-	equal(handlers.layout.sinon.callCount,0);
-	equal(handlers.discard.sinon.callCount,0);
+	// equal(handlers.enhance.sinon.callCount,0,"enhance should be completed already");
+	// equal(handlers.layout.sinon.callCount,0);
+	// equal(handlers.discard.sinon.callCount,0);
 
 
 	// Submit buttons turned into ordinary
@@ -111,7 +168,7 @@ test('Enhancing DocumentRoles',function(){
 	//TODO discard called for all enhanced
 
 	DocumentRoles.info.constructors[-1].discarded(dr); // emulate singleton teardown
-	equal(handlers.enhance.sinon.callCount,1);
-	equal(handlers.layout.sinon.callCount,0);
-	equal(handlers.discard.sinon.callCount,1);
+	// equal(handlers.enhance.sinon.callCount,1);
+	// equal(handlers.layout.sinon.callCount,0);
+	// equal(handlers.discard.sinon.callCount,1);
 });
