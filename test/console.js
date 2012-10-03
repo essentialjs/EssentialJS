a66691611d2a74200b25f0cc5e385d2cffbaf939
Thepian <henrik@thepia.com>
2012-10-03 15:51:52 +0200

    multiple variables in translate

--- test/console.js
+++ test/console.js
@@ -11,6 +11,12 @@ module("Translation tests");
 	translations.declare(["keys",null,"url","en"],"My URL [url]");
 	translations.declare(["phrases",null,"my url","en"],"My URL [url]");
 
+	translations.declare(["keys",null,"login.error","begin"],"[");
+	translations.declare(["keys",null,"login.error","end"],"]");
+	//TODO translations.declare(["keys",null,"login.error","defaults"],{ user:"?", url:"?"});
+	translations.declare(["keys",null,"login.error","en"],"Not logged in with [user] URL [url]");
+	translations.declare(["phrases",null,"login error","en"],"Not logged in with [user] URL [url]");
+
 })()
 
 test("Static Translations english",function(){
@@ -26,6 +32,10 @@ test("Static Translations english",function(){
 	equal(_({ phrase:"my abc" }),"My ABC")
 
 	equal(_("url",{ url: "http://abc.com"}),"My URL http://abc.com")
+
+	equal(_("login.error",{ url: "http://abc.com", user:"user1" }),"Not logged in with user1 URL http://abc.com")
+	equal(_("login.error",{ url: "http://abc.com" }),"Not logged in with [user] URL http://abc.com")
+	equal(_("login.error",{ user:"user1" }),"Not logged in with user1 URL [url]")
 
 	ok(1,"{cache:true, key:'abc' } saves resulting template on object")
 })
